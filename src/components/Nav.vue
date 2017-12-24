08c9d2f55705fbe09afebb9e0db3accd6a39b193
asaf bello <asafbello@gmail.com>
2017-12-24 19:46:00 +0200

    added routs

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -3,8 +3,8 @@
 
         <div class="line"></div>
             <el-menu  class="el-menu-demo" mode="horizontal" @select="handleSelect" background-color="#545c64" text-color="#fff" active-text-color="#ffd04b">
-            <el-menu-item index="3">Home</el-menu-item>
-            <el-menu-item index="2">Log In</el-menu-item>
+            <router-link to="/"> <el-menu-item index="3">Home</el-menu-item></router-link>
+            <router-link to="/user/:id"><el-menu-item index="2">Log In</el-menu-item></router-link>
 
             
             </el-menu>
