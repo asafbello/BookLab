96d80b8e04a25cbf93b3fa0a29d81ad51218a754
Nemo Frenkel <nafrenkel@gmail.com>
2017-12-25 09:53:21 +0200

    adding axios

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,5 +1,17 @@
 <template>
-<h1>home pageasasasasasa</h1>
+<div>
+  <section class="search-book">
+    <div>
+    <el-input :placeholder="'search and add '+ select" v-model="input5" class="input-with-select">
+      <el-select v-model="select" slot="prepend" placeholder="Select">
+        <el-option label="book" value="book"></el-option>
+        <el-option label="author" value="author"></el-option>
+      </el-select>
+      </el-input>
+    </div>
+      <el-button type="primary" icon="el-icon-search">Search</el-button>
+    </section>
+</div>
 </template>
 
 <script>
@@ -7,11 +19,25 @@ export default {
   name: 'HomePage',
   data () {
     return {
+      select: 'book',
+      input5: null
     }
   }
 }
 </script>
 
 <style scoped>
-
+  .search-book{
+    padding: 2vw 3vw;
+    display: flex;
+    flex-direction: row;
+    flex: 1;
+    justify-content: center;
+  }
+  .input-with-select{
+    width: 70vw;
+  }
+  .el-icon-search{
+    color: red;
+  }
 </style>
