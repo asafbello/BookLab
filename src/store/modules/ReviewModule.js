5cb5e6bd094455e18329979e42acf32a3664444d
asaf bello <asafbello@gmail.com>
2017-12-28 16:34:36 +0200

    added reading now in profile

--- src/store/modules/ReviewModule.js
+++ src/store/modules/ReviewModule.js
@@ -15,14 +15,14 @@ export default {
 
     actions: {
         [UPDATE_BOOK_AND_USER]({ rootState, store }, { objToUpdateBook, objToUpdateUser }) {
-            console.log('objToUpdateUser: ', objToUpdateUser);
+            // console.log('objToUpdateUser: ', objToUpdateUser);
             var userId = rootState.user.loggedinUser._id
             var bookId = rootState.book.currBook._id
-            console.log(objToUpdateBook, objToUpdateUser, userId, bookId);
+            // console.log(objToUpdateBook, objToUpdateUser, userId, bookId);
             ReviewService.updateBookAndUser(objToUpdateBook, objToUpdateUser, userId, bookId)
                 .then(res => {
                     this.commit({ type: UPDATE_USER, user: res[0] })
-                    this.commit({ type: UPDATE_BOOK, book: res[1] })
+                    // this.commit({ type: UPDATE_BOOK, book: res[1] })
                     saveToLocalStorage(res[0])
                     return res
                 })
