c0b63adc3f6986afd24f13b1e17fdd67f8539cab
Gordon <gordon.nadav@gmail.com>
2018-01-04 08:50:03 +0200

    morning merge

--- src/App.vue
+++ src/App.vue
@@ -38,5 +38,20 @@ export default {
     text-align: center;
     line-height: 60px;
   }
+
+  
+body::after {
+  background: url("./assets/img/cover.jpeg");
+  background-repeat: no-repeat;
+  content: "";
+  opacity: 0.45;
+  background-size: cover;
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  right: 0;
+  left: 0;
+  z-index: -1;
+}
   
 </style>
